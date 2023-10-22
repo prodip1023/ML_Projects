1f8f0fc7aaa4f66c592383de8386033d90837970
“prodip <prodip1023@gmail.com>
2023-10-22 12:17:51 +0530

    all updated setup and project folder

--- README.md
+++ README.md
@@ -1,9 +1,25 @@
-# This is my end to end project 
+# this is my end to end project
 
-# First initialize the Git
-<br>git init 
-<br> git add abc.txt 
- <br> git add .
- <br> git commit -m "my first commit"
- <br> git pull 
- <br> bash init_setup.sh
+# first initialize the git
+
+```
+git init
+```
+
+```
+git add abc.txt
+git add .
+```
+```
+git commit -m "this is my first commit"
+```
+
+```
+
+git pull
+
+```
+
+```
+bash your_file_name.sh
+```
--- init_setup.sh
+++ init_setup.sh
@@ -1,8 +1,18 @@
-echo [$(date)] : "START"
-echo [$(date)] : "creating env with python 3.8 version"
-conda create --prefix ./env python==3.8 -y
-echo [$(date)] : "activating the environment"
+echo [$(date)]: "START"
+
+
+echo [$(date)]: "creating env with python 3.8 version" 
+
+
+conda create --prefix ./env python=3.8 -y
+
+
+echo [$(date)]: "activating the environment" 
+
 source activate ./env
-echo [$(date)] : "Installing the dev requirements"
-pip install -r requirements.rxt
-echo [$(date)] : "END"
+
+echo [$(date)]: "installing the dev requirements" 
+
+pip install -r requirements.txt
+
+echo [$(date)]: "END" 
